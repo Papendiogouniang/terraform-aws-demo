f013fffbe4bd97992248b00498f101c4b7080cd7
Pndiogouniang <ndiogouniang01@gmail.com>
2026-01-28 18:17:33 +0100

    Fix: provider.tf utilise les variables d'environnement AWS

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-west-3"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.aws_access_key != "" ? var.aws_access_key : (try(env.AWS_ACCESS_KEY_ID, ""))
+  secret_key = var.aws_secret_key != "" ? var.aws_secret_key : (try(env.AWS_SECRET_ACCESS_KEY, ""))
 }
